9af473993dbe3749e7a7b003f361e6238f5b83a3
Fidelis <fabyo250@gmail.com>
2023-11-16 20:32:46 -0300

    vm configurada via ansible

---
+++ db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDF3rD/klaDAM7uEWdv2IDbyaQ9y1OofdmVfQu4+ulysm/2+3YvH2Mm2xE/5PrQj5K1oyfKKl65PYWiZ+9DhrgUXj8L+YHH10Wnl1gdbDpMfuIpqYsv0G1D5K/IUhbturBqO01KYI11VDLxhnhccbAw5rzOm4XGNa0p1iOUBaTu5knWE7YHnxydw5y5b1hpAhS9Auygo3jJbNqIGJERXVUhaH9EPDPG2m1MBAQDFatX0EwawikT8WVES3d7kF2g+sg/YZQKOdCib4hVRhuD1LJwH/UosuXyBQkwQOCwcvzFm+/fDKquCw+CzdXY4DBGn/o5jp9T1pDPnFQHJ4bvL3uB vagrant@control-node
+EOT
